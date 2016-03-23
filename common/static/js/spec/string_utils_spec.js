e80f6546aa7b323bf5907d7ef27a741dbbda57fa
Andy Armstrong <andya@edx.org>
2016-03-23 12:43:59 -0400

    Add static HtmlUtils/StringUtils into edx namespace
    This is a temporary workaround for code that needs
    access to these classes from the UI Toolkit but
    without using RequireJS.

--- common/static/js/spec/string_utils_spec.js
+++ common/static/js/spec/string_utils_spec.js
@@ -1,15 +1,280 @@
-describe('interpolate_ntext', function () {
-    it('replaces placeholder values', function () {
-        expect(interpolate_ntext('contains {count} student',  'contains {count} students', 1, {count: 1})).
-            toBe('contains 1 student');
-        expect(interpolate_ntext('contains {count} student',  'contains {count} students', 5, {count: 2})).
-            toBe('contains 2 students');
-    });
-});
+(function(edx) {
+    'use strict';
+    /**
+     * Runs func as a test case multiple times, using entries from data as arguments. Like Python's DDT.
+     * @param data An object mapping test names to arrays of function parameters. The name is passed to it() as the name
+     *      of the test case, and the list of arguments is applied as arguments to func.
+     * @param func The function that actually expresses the logic of the test.
+     */
+    var withData = function (data, func) {
+        /* jshint loopfunc:true */
+        for (var name in data) {
+            if (data.hasOwnProperty(name)) {
+                (function(name) {
+                    it(name, function() {
+                        func.apply(this, data[name]);
+                    });
+                })(name);
+            }
+        }
+    };
 
-describe('interpolate_text', function () {
-    it('replaces placeholder values', function () {
-        expect(interpolate_text('contains {adjective} students', {adjective: 'awesome'})).
-            toBe('contains awesome students');
+
+    describe('interpolate_ntext', function() {
+        it('replaces placeholder values', function() {
+            expect(interpolate_ntext('contains {count} student',  'contains {count} students', 1, {count: 1})).
+            toBe('contains 1 student');
+            expect(interpolate_ntext('contains {count} student',  'contains {count} students', 5, {count: 2})).
+            toBe('contains 2 students');
+        });
     });
-});
+
+    describe('interpolate_text', function () {
+        it('replaces placeholder values', function () {
+            expect(interpolate_text('contains {adjective} students', {adjective: 'awesome'})).
+            toBe('contains awesome students');
+        });
+    });
+
+    describe('edx.StringUtils', function() {
+        describe('interpolate', function() {
+            it('can interpolate a string with no parameters provided', function() {
+                expect(edx.StringUtils.interpolate('Hello, world')).toEqual(
+                    'Hello, world'
+                );
+            });
+
+            withData({
+                'can interpolate a string with empty parameters': [
+                    'Hello, world', {},
+                    'Hello, world'
+                ],
+                'can interpolate a string with one parameter': [
+                    'Hello, {name}', {name: 'Andy'},
+                    'Hello, Andy'
+                ],
+                'does not interpolate additional curly braces': [
+                    'Hello, {name}. Here is a { followed by a }', {name: 'Andy'},
+                    'Hello, Andy. Here is a { followed by a }'
+                ]
+            }, function(template, options, expectedString) {
+                var result = edx.StringUtils.interpolate(template, options);
+                expect(result).toEqual(expectedString);
+            });
+        });
+    });
+
+    describe('edx.HtmlUtils', function() {
+        beforeEach(function() {
+            setFixtures('<div class="test"></div>');
+        });
+
+        describe('HtmlSnippet', function() {
+            it('can convert to a string', function() {
+                expect(new edx.HtmlUtils.HtmlSnippet('Hello, world').toString()).toBe('Hello, world');
+            });
+        });
+
+        describe('HTML', function() {
+            it('returns an HTML snippet', function() {
+                expect(edx.HtmlUtils.HTML('Hello, world') instanceof edx.HtmlUtils.HtmlSnippet).toBeTruthy();
+            });
+        });
+
+        describe('ensureHtml', function() {
+            withData({
+                'HTML escapes text strings': [
+                    'Rock & Roll',
+                    'Rock &amp; Roll'
+                ],
+                'HTML escapes full HTML strings': [
+                    '<a href="world">Rock &amp; Roll</a>',
+                    '&lt;a href=&quot;world&quot;&gt;Rock &amp;amp; Roll&lt;/a&gt;'
+                ],
+                'does not escape HTML snippets': [
+                    edx.HtmlUtils.HTML('<a href="world">Rock &amp; Roll</a>'),
+                    '<a href="world">Rock &amp; Roll</a>'
+                ]
+            }, function(input, expectedString) {
+                var result = edx.HtmlUtils.ensureHtml(input);
+                expect(result instanceof edx.HtmlUtils.HtmlSnippet).toBeTruthy();
+                expect(result.toString()).toEqual(expectedString);
+            });
+        });
+
+        describe('interpolateHtml', function() {
+            it('can interpolate a string with no parameters provided', function() {
+                expect(edx.HtmlUtils.interpolateHtml('Hello, world').toString()).toEqual(
+                    'Hello, world'
+                );
+            });
+
+            withData({
+                'can interpolate a string with empty parameters': [
+                    'Hello, world', {},
+                    'Hello, world'
+                ],
+                'can interpolate a string with one parameter': [
+                    'Hello, {name}', {name: 'Andy'},
+                    'Hello, Andy'
+                ],
+                'does not interpolate additional curly braces': [
+                    'Hello, {name}. Here is a { followed by a }', {name: 'Andy'},
+                    'Hello, Andy. Here is a { followed by a }'
+                ],
+                'escapes characters in the template': [
+                    'Rock & Roll', {},
+                    'Rock &amp; Roll'
+                ],
+                'does not escape HTML parameters': [
+                    'Hello, {anchor}', {anchor: edx.HtmlUtils.HTML('<a href="world">world</a>')},
+                    'Hello, <a href="world">world</a>'
+                ],
+                'escapes characters in parameters': [
+                    'I love {name}', {name: 'Rock & Roll'},
+                    'I love Rock &amp; Roll'
+                ],
+                'does not double escape when chaining interpolate calls': [
+                    'I love {name}', {name: edx.HtmlUtils.interpolateHtml('Rock & Roll')},
+                    'I love Rock &amp; Roll'
+                ],
+                'full example': [
+                    'You are enrolling in {spanStart}{courseName}{spanEnd}',
+                    {
+                        courseName: 'Rock & Roll',
+                        spanStart: edx.HtmlUtils.HTML('<span class="course-title">'),
+                        spanEnd: edx.HtmlUtils.HTML('</span>')
+                    },
+                    'You are enrolling in <span class="course-title">Rock &amp; Roll</span>'
+                ]
+            }, function(template, options, expectedString) {
+                var result = edx.HtmlUtils.interpolateHtml(template, options);
+                expect(result instanceof edx.HtmlUtils.HtmlSnippet).toBeTruthy();
+                expect(result.toString()).toEqual(expectedString);
+            });
+        });
+
+        describe('joinHtml', function() {
+            withData({
+                'can join a single string': [
+                    ['Hello, world'],
+                    'Hello, world'
+                ],
+                'escapes characters provided as strings': [
+                    ['Rock & Roll'],
+                    'Rock &amp; Roll'
+                ],
+                'does not escape HTML snippets': [
+                    [edx.HtmlUtils.HTML('<a href="world">world</a>')],
+                    '<a href="world">world</a>'
+                ],
+                'can join a mixture of strings and HTML snippets': [
+                    ['Rock & Roll', ' all over the ', edx.HtmlUtils.HTML('<a href="world">world</a>')],
+                    'Rock &amp; Roll all over the <a href="world">world</a>'
+                ]
+            }, function(items, expectedString) {
+                var result = edx.HtmlUtils.joinHtml.apply(this, items);
+                expect(result instanceof edx.HtmlUtils.HtmlSnippet).toBeTruthy();
+                expect(result.toString()).toEqual(expectedString);
+            });
+        });
+
+        describe('template', function() {
+            it('can render a template with no parameters', function() {
+                var template = edx.HtmlUtils.template('Hello, world'),
+                    result = template();
+                expect(result instanceof edx.HtmlUtils.HtmlSnippet).toBeTruthy();
+                expect(result.toString()).toEqual('Hello, world');
+            });
+
+            it('can render a template with parameters', function() {
+                var template = edx.HtmlUtils.template('Hello, <%- name %>'),
+                    result = template({name: 'world'});
+                expect(result instanceof edx.HtmlUtils.HtmlSnippet).toBeTruthy();
+                expect(result.toString()).toEqual('Hello, world');
+            });
+
+            it('adds HtmlView as an additional context variable for the template', function() {
+                var template = edx.HtmlUtils.template('I love <%= HtmlUtils.ensureHtml("Rock & Roll") %>');
+                expect(template().toString()).toEqual('I love Rock &amp; Roll');
+            });
+        });
+
+        describe('setHtml', function() {
+            withData({
+                'HTML escapes text strings': [
+                    'Rock & Roll',
+                    'Rock &amp; Roll'
+                ],
+                'HTML escapes full HTML strings': [
+                    '<a href="world">Rock &amp; Roll</a>',
+                    '&lt;a href="world"&gt;Rock &amp;amp; Roll&lt;/a&gt;'
+                ],
+                'does not escape HTML snippets': [
+                    edx.HtmlUtils.HTML('<a href="world">Rock &amp; Roll</a>'),
+                    '<a href="world">Rock &amp; Roll</a>'
+                ]
+            }, function(input, expectedString) {
+                var $element = $('.test');
+                edx.HtmlUtils.setHtml($element, input);
+                expect($element.html()).toEqual(expectedString);
+            });
+        });
+
+        describe('append', function() {
+            withData({
+                'HTML escapes text strings': [
+                    'Rock & Roll',
+                    'Rock &amp; Roll'
+                ],
+                'HTML escapes full HTML strings': [
+                    '<a href="world">Rock &amp; Roll</a>',
+                    '&lt;a href="world"&gt;Rock &amp;amp; Roll&lt;/a&gt;'
+                ],
+                'does not escape HTML snippets': [
+                    edx.HtmlUtils.HTML('<a href="world">Rock &amp; Roll</a>'),
+                    '<a href="world">Rock &amp; Roll</a>'
+                ]
+            }, function(input, expectedString) {
+                var $element = $('.test');
+
+                // Appends correctly with no children
+                edx.HtmlUtils.append($element, input);
+                expect($element.html()).toEqual(expectedString);
+
+                // Appends correctly with a pre-existing child
+                $element.html('<p>Hello, world</p>');
+                edx.HtmlUtils.append($element, input);
+                expect($element.html()).toEqual('<p>Hello, world</p>' + expectedString);
+            });
+        });
+
+        describe('prepend', function() {
+            withData({
+                'HTML escapes text strings': [
+                    'Rock & Roll',
+                    'Rock &amp; Roll'
+                ],
+                'HTML escapes full HTML strings': [
+                    '<a href="world">Rock &amp; Roll</a>',
+                    '&lt;a href="world"&gt;Rock &amp;amp; Roll&lt;/a&gt;'
+                ],
+                'does not escape HTML snippets': [
+                    edx.HtmlUtils.HTML('<a href="world">Rock &amp; Roll</a>'),
+                    '<a href="world">Rock &amp; Roll</a>'
+                ]
+            }, function(input, expectedString) {
+                var $element = $('.test');
+
+                // Prepends correctly with no children
+                edx.HtmlUtils.prepend($element, input);
+                expect($element.html()).toEqual(expectedString);
+
+                // Prepends correctly with a pre-existing child
+                $element.html('<p>Hello, world</p>');
+                edx.HtmlUtils.prepend($element, input);
+                expect($element.html()).toEqual(expectedString + '<p>Hello, world</p>');
+            });
+        });
+    });
+})(edx);
--- common/static/js/src/string_utils.js
+++ common/static/js/src/string_utils.js
@@ -1,3 +1,5 @@
+var edx = edx || {};
+
 // String utility methods.
 (function(_) {
     /**
@@ -45,4 +47,277 @@
         return _.template(text, {interpolate: /\{(.+?)\}/g})(values);
     };
     this.interpolate_text = interpolate_text;
+
+    edx.StringUtils = (function() {
+        var interpolate;
+
+        /**
+         * Returns a string created by interpolating the provided parameters.
+         *
+         * The HTML text is provided as a tokenized format string where parameters
+         * are indicated via curly braces, e.g. 'Hello {name}'. These tokens are
+         * replaced by the parameter value of the same name.
+         *
+         * Parameter values will be rendered using their toString methods and then
+         * HTML-escaped. The only exception is that instances of the class HTML
+         * are rendered without escaping as their contract declares that they are
+         * already valid HTML.
+         *
+         * Example:
+         *   HtmlUtils.interpolate(
+         *       'You are enrolling in {spanStart}{courseName}{spanEnd}',
+         *       {
+         *           courseName: 'Rock & Roll 101',
+         *           spanStart: HtmlUtils.HTML('<span class="course-title">'),
+         *           spanEnd: HtmlUtils.HTML('</span>')
+         *       }
+         *   );
+         *
+         * returns:
+         *   'You are enrolling in <span class="course-title">Rock &amp; Roll 101</span>'
+         *
+         * Note: typically the formatString will need to be internationalized, in which
+         * case it will be wrapped with a call to an i18n lookup function. In Django,
+         * this would look like:
+         *
+         *   HtmlUtils.interpolate(
+         *       gettext('You are enrolling in {spanStart}{courseName}{spanEnd}'),
+         *       ...
+         *   );
+         *
+         * @param {string} formatString The string to be interpolated.
+         * @param {Object} parameters An optional set of parameters to the template.
+         * @returns {string} A string with the values interpolated.
+         */
+        interpolate = function(formatString, parameters) {
+            return formatString.replace(/{\w+}/g,
+                function(parameter) {
+                    var parameterName = parameter.slice(1,-1);
+                    return String(parameters[parameterName]);
+                });
+        };
+
+        return {
+            interpolate: interpolate
+        };
+    })();
+
+    /**
+     * Temporary clone of the UI Toolkit's HtmlUtils class.
+     *
+     * This is for use by code that does not use RequireJS and hence can't load HtmlUtils
+     * dynamically.
+     */
+    edx.HtmlUtils = (function() {
+        var ensureHtml, interpolateHtml, joinHtml, HTML, template, setHtml, append, prepend;
+
+        /**
+         * Creates an HTML snippet.
+         *
+         * The intention of an HTML snippet is to communicate that the string
+         * it represents contains HTML that has been safely escaped as necessary.
+         * As an example, this allows interpolate to understand that
+         * it does not need to further escape this HTML.
+         *
+         * @param {string} htmlString The string of HTML.
+         * @constructor
+         */
+        function HtmlSnippet(htmlString) {
+            this.text = htmlString;
+        }
+        HtmlSnippet.prototype.valueOf = function() {
+            return this.text;
+        };
+        HtmlSnippet.prototype.toString = function() {
+            return this.text;
+        };
+
+        /**
+         * Helper function to create an HTML snippet from a string.
+         *
+         * The intention of an HTML snippet is to communicate that the string
+         * it represents contains HTML that has been safely escaped as necessary.
+         *
+         * @param {string} htmlString The string of HTML.
+         * @returns {HtmlSnippet} An HTML snippet that can be safely rendered.
+         * @constructor
+         */
+        HTML = function(htmlString) {
+            return new HtmlSnippet(htmlString);
+        };
+
+        /**
+         * Ensures that the provided text is properly HTML-escaped.
+         *
+         * If a plain text string is provided, then it will be HTML-escaped and
+         * returned as an HtmlSnippet. If the parameter is an HTML snippet
+         * then it will be returned directly so as not to double escape it.
+         *
+         * @param {(string|HtmlSnippet)} html Either a plain text string
+         * or an HTML snippet.
+         * @returns {HtmlSnippet} A safely escaped HTML snippet.
+         */
+        ensureHtml = function(html) {
+            if (html instanceof HtmlSnippet) {
+                return html;
+            } else {
+                return HTML(_.escape(html));
+            }
+        };
+
+        /**
+         * Returns an HTML snippet by interpolating the provided parameters.
+         *
+         * The text is provided as a tokenized format string where parameters
+         * are indicated via curly braces, e.g. 'Hello {name}'. These tokens are
+         * replaced by the parameter value of the same name.
+         *
+         * Parameter values will be rendered using their toString methods and then
+         * HTML-escaped. The only exception is that instances of the class HTML
+         * are rendered without escaping as their contract declares that they are
+         * already valid HTML.
+         *
+         * Example:
+         *   HtmlUtils.interpolateHtml(
+         *       'You are enrolling in {spanStart}{courseName}{spanEnd}',
+         *       {
+         *           courseName: 'Rock & Roll 101',
+         *           spanStart: HtmlUtils.HTML('<span class="course-title">'),
+         *           spanEnd: HtmlUtils.HTML('</span>')
+         *       }
+         *   );
+         *
+         * returns:
+         *   'You are enrolling in <span class="course-title">Rock &amp; Roll 101</span>'
+         *
+         * Note: typically the formatString will need to be internationalized, in which
+         * case it will be wrapped with a call to an i18n lookup function. In Django,
+         * this would look like:
+         *
+         *   HtmlUtils.interpolateHtml(
+         *       gettext('You are enrolling in {spanStart}{courseName}{spanEnd}'),
+         *       ...
+         *   );
+         *
+         * @param {string} formatString The string to be interpolated.
+         * @param {Object} parameters An optional set of parameters for interpolation.
+         * @returns {HtmlSnippet} The resulting safely escaped HTML snippet.
+         */
+        interpolateHtml = function(formatString, parameters) {
+            var result = edx.StringUtils.interpolate(
+                ensureHtml(formatString).toString(),
+                _.mapObject(parameters, ensureHtml)
+            );
+            return HTML(result);
+        };
+
+        /**
+         * Joins multiple strings and/or HTML snippets together to produce
+         * a single safely escaped HTML snippet.
+         *
+         * For each item, if it is provided as an HTML snippet then it is joined
+         * directly. If the item is a string then it is assumed to be unescaped and
+         * so it is first escaped before being joined.
+         *
+         * @param {...(string|HtmlSnippet)} items The strings and/or HTML snippets
+         * to be joined together.
+         * @returns {HtmlSnippet} The resulting safely escaped HTML snippet.
+         */
+        joinHtml = function() {
+            var html = '',
+                argumentCount = arguments.length,
+                i;
+            for (i = 0; i < argumentCount; i++) {
+                html += ensureHtml(arguments[i]);
+            }
+            return HTML(html);
+        };
+
+        /**
+         * Returns a function that renders an Underscore template as an HTML snippet.
+         *
+         * Note: This helper function makes the following context parameters
+         * available to the template in addition to those passed in:
+         *
+         *   * HtmlUtils: the HtmlUtils helper class
+         *
+         * @param {string} text
+         * @param {object} settings
+         * @returns {function} A function that returns a rendered HTML snippet.
+         */
+        template = function(text, settings) {
+            var HtmlUtils = this,
+                rawTemplate = _.template(text, settings),
+                template = function(data) {
+                    var augmentedData = _.extend(
+                        {
+                            HtmlUtils: HtmlUtils
+                        },
+                        data || {}
+                    );
+                    return HTML(rawTemplate(augmentedData));
+                };
+            return template;
+        };
+
+        /**
+         * A wrapper for $.html that safely escapes the provided HTML.
+         *
+         * If the HTML is provided as an HTML snippet then it is used directly.
+         * If the value is a string then it is assumed to be unescaped and
+         * so it is first escaped before being used.
+         *
+         * @param {element} element The element or elements to be updated.
+         * @param {(string|HtmlSnippet)} html The desired HTML, either as a
+         * plain string or as an HTML snippet.
+         * @returns {JQuery} The JQuery object representing the element or elements.
+         */
+        setHtml = function(element, html) {
+            return $(element).html(ensureHtml(html).toString());
+        };
+
+        /**
+         * A wrapper for $.append that safely escapes the provided HTML.
+         *
+         * If the HTML is provided as an HTML snippet then it is used directly.
+         * If the value is a string then it is assumed to be unescaped and
+         * so it is first escaped before being used.
+         *
+         * @param {element} element The element or elements to be updated.
+         * @param {(string|HtmlSnippet)} html The desired HTML, either as a
+         * plain string or as an HTML snippet.
+         * @returns {JQuery} The JQuery object representing the element or elements.
+         */
+        append = function(element, html) {
+            return $(element).append(ensureHtml(html).toString());
+        };
+
+        /**
+         * A wrapper for $.prepend that safely escapes the provided HTML.
+         *
+         * If the HTML is provided as an HTML snippet then it is used directly.
+         * If the value is a string then it is assumed to be unescaped and
+         * so it is first escaped before being used.
+         *
+         * @param {element} element The element or elements to be updated.
+         * @param {(string|HtmlSnippet)} html The desired HTML, either as a
+         * plain string or as an HTML snippet.
+         * @returns {JQuery} The JQuery object representing the element or elements.
+         */
+        prepend = function(element, html) {
+            return $(element).prepend(ensureHtml(html).toString());
+        };
+
+        return {
+            append: append,
+            ensureHtml: ensureHtml,
+            HTML: HTML,
+            HtmlSnippet: HtmlSnippet,
+            interpolateHtml: interpolateHtml,
+            joinHtml: joinHtml,
+            prepend: prepend,
+            setHtml: setHtml,
+            template: template
+        };
+    })();
 }).call(this, _);
