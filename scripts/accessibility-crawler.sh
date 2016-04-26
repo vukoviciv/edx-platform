8d27b92901ae425f3f352dfabb18637d4a9630de
Christine Lytwynec <clytwynec@edx.org>
2016-04-26 13:39:21 -0400

    run pa11ycrawler with --fasttest

--- scripts/accessibility-crawler.sh
+++ scripts/accessibility-crawler.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Setting up for accessibility tests..."
-source scripts/jenkins-common.sh
+# Note: It is assumed that this runs after accessibility_tests.sh
+# so we do not run jenkins-common.sh again. If we decide to run
+# this on PR builds, we can just move the following to accessibility-tests.sh
 
 echo "Running pa11ycrawler against test course..."
-paver pa11ycrawler
+paver pa11ycrawler --fasttest
 
 echo "Generating coverage report..."
 paver pa11ycrawler_coverage
